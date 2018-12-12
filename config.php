e4716ee73bade02f1d6f0220f12c20f3b9e7bec0
min <gmin1ing@icloud.com>
2018-12-12 16:23:48 +0800

    抽象公共部分

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 项目配置文件
+ *
+ */
+
+/**
+   * 数据库主机
+ */
+define('DB_HOST','localhost');
+/**
+   * 数据库用户名
+ */
+define('DB_USER', 'root');
+/**
+   * 数据库密码
+ */
+define('DB_PASSWORD', '123456');
+/**
+   * 数据库名
+ */
+define('DB_NAME', 'baixiu');
+
