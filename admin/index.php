2fcd83dbe6855def6a70b449919380bdd36f22f9
min <gmin1ing@icloud.com>
2018-12-13 14:48:11 +0800

    08 封装数据库查询 及图表使用

--- admin/index.php
+++ admin/index.php
@@ -7,7 +7,16 @@
   //    header('Location: /admin/login.php');
   // }
 require_once '../functions.php';
-xiu_get_current_user();
+
+xiu_get_current_user(); // 判断用户是否登录一定是最先做的事情
+
+// 获取界面所需的数据，重复的操作一定封装
+$posts_count = xiu_fetch_one("select count(1) as num from posts;")['num'];
+$categories_count = xiu_fetch_one("select count(1) as num from categories;")['num'];
+$comments_count = xiu_fetch_one("select count(1) as num from comments;")['num'];
+$drafted_count = xiu_fetch_one("select count(1) as num from posts where status = 'drafted';")['num'];
+
+$held_count = xiu_fetch_one("select count(1) as num from comments where status = 'held';")['num'];
 
 ?>
 
@@ -41,13 +50,15 @@ xiu_get_current_user();
               <h3 class="panel-title">站点内容统计：</h3>
             </div>
             <ul class="list-group">
-              <li class="list-group-item"><strong>10</strong>篇文章（<strong>2</strong>篇草稿）</li>
-              <li class="list-group-item"><strong>6</strong>个分类</li>
-              <li class="list-group-item"><strong>5</strong>条评论（<strong>1</strong>条待审核）</li>
+              <li class="list-group-item"><strong><?php echo $posts_count;  ?></strong>篇文章（<strong><?php echo $drafted_count; ?></strong>篇草稿）</li>
+              <li class="list-group-item"><strong><?php echo $categories_count; ?></strong>个分类</li>
+              <li class="list-group-item"><strong><?php echo $comments_count; ?></strong>条评论（<strong><?php echo $held_count; ?></strong>条待审核）</li>
             </ul>
           </div>
         </div>
-        <div class="col-md-4"></div>
+        <div class="col-md-4">
+          <canvas id="myChart"></canvas>
+        </div>
         <div class="col-md-4"></div>
       </div>
     </div>
@@ -58,6 +69,33 @@ xiu_get_current_user();
 
   <script src="/static/assets/vendors/jquery/jquery.js"></script>
   <script src="/static/assets/vendors/bootstrap/js/bootstrap.js"></script>
+  <script src="/static/assets/vendors/chart/chart.js"></script>
   <script>NProgress.done()</script>
+  <script>
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myPieChart = new Chart(ctx,{
+            type: 'pie',
+            data: {
+                    // These labels appear in the legend and in the tooltips when hovering different arcs
+                    labels: ["文章数", "分类数", "评论数"],
+                    datasets: [{
+                          // label: '# of Votes',
+                          data: [<?php echo $posts_count; ?>, <?php echo $categories_count; ?>, <?php echo $comments_count; ?>],
+                          backgroundColor: [
+                                'rgba(255, 99, 132, 0.5)',
+                                'rgba(54, 162, 235, 0.5)',
+                                'rgba(255, 206, 86, 0.5)'
+                                ],
+                          borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)'
+                                 ],
+                          borderWidth: 1  
+                          }],
+            },
+            
+    });
+  </script>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -1,10 +1,9 @@
 <?php
-
+require_once '../config.php';
 /**
  * 封装公用的函数
  * 
  */
-
 
 
 /**
@@ -21,4 +20,37 @@ function xiu_get_current_user(){
 		exit();// 没有登录过没有必要在执行了
 	}
 	return $_SESSION['current_login_user'];
+}
+
+
+/**
+ * 通过一个数据库查询获取多条数据
+ * @return 索引数据嵌套关联数组
+ */
+function xiu_fetch_all($sql){// 获取多条数据
+	// $conn = mysqli_connect()('127.0.0.1', 'root', '123456', 'baixiu');
+	$conn= mysqli_connect(XIU_DB_HOST, XIU_DB_USER, XIU_DB_PASSWORD, XIU_DB_NAME);
+	if (!$conn) {
+		exit('数据库连接失败');
+	}
+	$query = mysqli_query($conn, $sql);
+	if (!$query) {// 查询失败
+		return false;
+	}
+	while ($row = mysqli_fetch_assoc($query)) {
+		$result[]= $row;
+	}
+	mysqli_free_result($query);
+	mysqli_close($conn);
+	return $result;
+
+}
+
+/**
+ * 通过一个数据库查询获取一条数据
+ * @return 关联数组
+ */
+function xiu_fetch_one($sql){// 获取单条数据
+	$res = xiu_fetch_all($sql);
+	return isset($res[0]) ? $res[0] : null;
 }
